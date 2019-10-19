b1c6ba3403af9fb57ed2f1660c074f3c6a49b1eb
LennyRamos <newguyintownlr7@gmail.com>
2019-10-18 21:42:25 -0400

    custom Logger works

--- src/utils/Log.kt
+++ src/utils/Log.kt
@@ -3,9 +3,11 @@ package utils
 /**
  * This will be the custom logger for use in this project
  */
-class Log <T> constructor(classType: Class<T>) {
+class Log<T> constructor(classType: Class<in T>) {
 
-    fun write() {
-        println()
+    private val s: String = "[" + classType.simpleName + "] : "
+
+    fun write(input: String) {
+        println(s + input)
     }
 }
