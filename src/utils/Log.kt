3f8ed718b099eec0cd12888a84e8c6610ba995ed
LennyRamos <newguyintownlr7@gmail.com>
2019-10-18 21:11:43 -0400

    creating custom Logger

---
+++ src/utils/Log.kt
@@ -0,0 +1,11 @@
+package utils
+
+/**
+ * This will be the custom logger for use in this project
+ */
+class Log <T> constructor(classType: Class<T>) {
+
+    fun write() {
+        println()
+    }
+}
