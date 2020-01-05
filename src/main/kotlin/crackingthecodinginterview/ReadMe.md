9100f5b02834d934478f3d67f9c5abc589dfb2c7
LennyRamos <tuf18062@temple.edu>
2020-01-04 19:06:43 -0500

    pushing up new files

---
+++ src/main/kotlin/crackingthecodinginterview/ReadMe.md
@@ -0,0 +1,8 @@
+This directory is for the purpose of learning. 
+
+
+There will be two files per question/answer. 
+1 in Java another in Kotlin 
+
+I will write out my solution first then will write out what is "best or standard" solution 
+for the problem.
---
+++ src/main/kotlin/crackingthecodinginterview/arraysandstrings/IsUnique1_1.java
@@ -0,0 +1,14 @@
+package crackingthecodinginterview.arraysandstrings;
+
+/**
+ * Question : Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ *
+ * @author Lenny Ramos
+ */
+public class IsUnique1_1 {
+
+    public IsUnique1_1() {
+
+    }
+}
